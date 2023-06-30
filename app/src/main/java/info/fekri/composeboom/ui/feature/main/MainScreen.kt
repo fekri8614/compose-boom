2a9f958fbe35ac60add55adc643f302c8c078ddf
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-06-30 01:25:57 -0700

    added top part of `MainScreen`

--- app/src/main/java/info/fekri/composeboom/ui/feature/main/MainScreen.kt
+++ app/src/main/java/info/fekri/composeboom/ui/feature/main/MainScreen.kt
@@ -1,16 +1,156 @@
 package info.fekri.composeboom.ui.feature.main
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dev.burnoo.cokoin.navigation.getNavController
+import info.fekri.composeboom.R
+import info.fekri.composeboom.ui.theme.PrimaryDarkColor
+import info.fekri.composeboom.ui.theme.YellowBackground
+import me.onebone.toolbar.CollapsingToolbarScaffold
+import me.onebone.toolbar.ScrollStrategy
+import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val context = LocalContext.current
+    val uiController = rememberSystemUiController()
+    SideEffect {
+        uiController.setStatusBarColor(PrimaryDarkColor)
+    }
     val navigation = getNavController()
 //    val viewModel = getNavViewModel<MainScreenViewModel>()
+    val state = rememberCollapsingToolbarScaffoldState()
 
+    CollapsingToolbarScaffold(
+        modifier = modifier.fillMaxSize(),
+        state = state,
+        scrollStrategy = ScrollStrategy.EnterAlwaysCollapsed,
+        toolbar = {
+            val offsetY = state.offsetY
+            val progress = state.toolbarState.progress
 
+            Box {
+                Box {
+                    Image(
+                        painter = painterResource(id = R.drawable.img1),
+                        contentDescription = null,
+                        modifier = modifier
+                            .fillMaxSize()
+                            .pin(),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = modifier.align(Alignment.Center)
+                ) {
+                    Text(
+                        text = "Boom!",
+                        style = TextStyle(
+                            fontSize = (20 + (30 - 18) * progress).sp,
+                            fontWeight = FontWeight.Medium,
+                            color = Color.White,
+                            textAlign = TextAlign.Justify
+                        ),
+                    )
+                    Text(
+                        text = "Your book-owl friend!",
+                        style = TextStyle(
+                            fontSize = (20 + (30 - 18) * progress).sp,
+                            fontWeight = FontWeight.Medium,
+                            color = Color.White,
+                            textAlign = TextAlign.Justify
+                        ),
+                    )
+                }
+            }
+        },
+        toolbarModifier = modifier.verticalScroll(rememberScrollState())
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(8.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TopCircularIcons()
+
+        }
+    }
 
 }
+
+// -----------------------------------------------------------
+
+@Composable
+fun TopCircularIcons() {
+    Row(horizontalArrangement = Arrangement.SpaceAround, modifier = Modifier.fillMaxWidth()) {
+        CircularIcon(img = R.drawable.img_all, title = "all") {}
+        CircularIcon(img = R.drawable.img_audio_lib, title = "voice lib") {}
+        CircularIcon(img = R.drawable.img_watch_list, title = "video lib") {}
+        CircularIcon(img = R.drawable.img_library_all, title = "photo lib") {}
+    }
+}
+
+@Composable
+fun CircularIcon(modifier: Modifier = Modifier, img: Int, title: String, onIconClicked: () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+        .padding(8.dp)
+        .clickable { onIconClicked.invoke() }) {
+        Card(
+            shape = RoundedCornerShape(64.dp),
+            elevation = 4.dp,
+            border = BorderStroke(1.dp, YellowBackground),
+            modifier = modifier.size(70.dp)
+        ) {
+            Image(
+                painter = painterResource(id = img),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+        }
+        Spacer(modifier = modifier.height(8.dp))
+        Text(
+            text = title,
+            style = TextStyle(
+                color = Color.Black,
+                        fontWeight = FontWeight.Medium,
+                fontSize = 18.sp
+            )
+        )
+    }
+}
+
+// -----------------------------------------------------------
