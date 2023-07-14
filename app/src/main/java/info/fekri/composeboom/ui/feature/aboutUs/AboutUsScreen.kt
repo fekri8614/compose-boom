8a303170d786b6cd44aa2b083b47fcab9a151f46
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-07-14 01:13:02 -0700

    finished UI of `AboutUsScreen`

--- app/src/main/java/info/fekri/composeboom/ui/feature/aboutUs/AboutUsScreen.kt
+++ app/src/main/java/info/fekri/composeboom/ui/feature/aboutUs/AboutUsScreen.kt
@@ -3,7 +3,10 @@ package info.fekri.composeboom.ui.feature.aboutUs
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,7 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
@@ -21,11 +24,11 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -33,15 +36,16 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dev.burnoo.cokoin.navigation.getNavController
 import info.fekri.composeboom.R
+import info.fekri.composeboom.ui.feature.main.CircularIcon
 import info.fekri.composeboom.ui.theme.BlueBackground
+import info.fekri.composeboom.ui.theme.BlueLightBack
 import info.fekri.composeboom.ui.theme.PrimaryDarkColor
-import info.fekri.composeboom.util.IconMainApp
+import info.fekri.composeboom.util.ABOUT_US_ITEMS
 import info.fekri.composeboom.util.MyScreens
 import info.fekri.composeboom.util.NetworkChecker
 
@@ -51,7 +55,6 @@ fun AboutUsScreen() {
     SideEffect {
         uiController.setStatusBarColor(PrimaryDarkColor)
     }
-
     val context = LocalContext.current
     val navigation = getNavController()
 
@@ -63,29 +66,63 @@ fun AboutUsScreen() {
         }
 
         BodyAboutUs()
-
     }
-
 }
 
 // -----------------------------------------------------
-
+@Preview
 @Composable
 fun BodyAboutUs() {
     Column(
         modifier = Modifier.padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
     ) {
         TopImagePart()
 
-        SecondInfoContent()
+        Spacer(modifier = Modifier.height(80.dp))
+
+        FirstInfoContent()
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        SecondInfoContact()
     }
 }
 
 @Composable
+fun SecondInfoContact() {
+    LazyRow(horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+        items(ABOUT_US_ITEMS.size) {
+            CircularInfo(
+                ABOUT_US_ITEMS[it].first,
+                ABOUT_US_ITEMS[it].second
+            )
+        }
+    }
+}
+
+@Composable
+fun CircularInfo(text: String, txtColor: Color = Color.White) {
+    Card(
+        shape = RoundedCornerShape(70),
+        backgroundColor = BlueLightBack,
+        elevation = 4.dp,
+        border = BorderStroke(2.dp, Color.White)
+    ) {
+        Column {
+            Text(text = text, color = txtColor, fontWeight = FontWeight.Medium, fontSize = 16.sp)
+        }
+    }
+}
+
+// -------------------------------------------------------
+
+@Composable
 fun TopImagePart() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
     ) {
         Card(
             elevation = 5.dp,
@@ -119,12 +156,29 @@ fun TopImagePart() {
         )
         
     }
-
 }
 
-@Composable
-fun SecondInfoContent() {
+// ----------------------------------------------------------
 
+@Composable
+fun FirstInfoContent() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(
+            modifier = Modifier
+                .border(2.dp, Color.White, shape = RoundedCornerShape(20))
+                .fillMaxWidth()
+                .background(BlueLightBack, shape = RoundedCornerShape(20))
+        ) {
+            Text(
+                text = "Compose Boom! is a cloned version of Boom book library application. The aim of developing this application is to develop a sample version of the real applications. This application is developed by Mohammad Reza Fekri. Contact me by:",
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    color = Color.Black
+                ),
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
 }
 
 // -----------------------------------------------
--- app/src/main/java/info/fekri/composeboom/ui/theme/Color.kt
+++ app/src/main/java/info/fekri/composeboom/ui/theme/Color.kt
@@ -6,5 +6,6 @@ val BackgroundMain = Color(0xFFF8F2E4)
 val YellowBackground = Color(0xFFE8BC15)
 val GreenBackground = Color(0xFF3342F554)
 val BlueBackground = Color(0xFF3342A5F5)
+val BlueLightBack = Color(66,165,245,20)
 
 val PrimaryDarkColor = Color(0xFF3D8B97)
