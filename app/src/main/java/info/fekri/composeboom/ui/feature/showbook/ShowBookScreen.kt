87fe0c5a418c7efeaded47971557de61e0b69e4d
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-07-19 05:52:20 -0700

    completed UI of `ShowBookScreen`

--- app/src/main/java/info/fekri/composeboom/ui/feature/showbook/ShowBookScreen.kt
+++ app/src/main/java/info/fekri/composeboom/ui/feature/showbook/ShowBookScreen.kt
@@ -1,10 +1,16 @@
 package info.fekri.composeboom.ui.feature.showbook
 
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,7 +19,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
@@ -26,6 +34,10 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,13 +45,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dev.burnoo.cokoin.navigation.getNavController
-import dev.burnoo.cokoin.navigation.getNavViewModel
 import info.fekri.composeboom.R
 import info.fekri.composeboom.ui.theme.BackgroundMain
 import info.fekri.composeboom.ui.theme.BlueLightBack
@@ -47,30 +59,34 @@ import info.fekri.composeboom.ui.theme.PrimaryDarkColor
 import info.fekri.composeboom.ui.theme.YellowBackground
 
 @Composable
-fun ShowBookScreen(bookId: String) {
+fun ShowBookScreen(/*bookId: String = ""*/) {
     val uiController = rememberSystemUiController()
     SideEffect {
         uiController.setStatusBarColor(PrimaryDarkColor)
     }
 
     val context = LocalContext.current
-    val viewModel = getNavViewModel<ShowBookViewModel>()
-    viewModel.getDataBookFromNet(bookId)
+//    val viewModel = getNavViewModel<ShowBookViewModel>()
+//    viewModel.getDataBookFromNet(bookId)
 
     val navigation = getNavController()
 
     Scaffold(
         scaffoldState = rememberScaffoldState(),
         backgroundColor = BackgroundMain,
-        topBar = { ShowBookTopBar("book_title") {
-            navigation.popBackStack()
-        } }
+        topBar = {
+            ShowBookTopBar("book_title") {
+                navigation.popBackStack()
+            }
+        }
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(it)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
         ) {
             AboutUsBody()
         }
@@ -80,11 +96,16 @@ fun ShowBookScreen(bookId: String) {
 
 @Composable
 fun AboutUsBody() {
+    var showContent by remember { mutableStateOf(false) }
+    val transition = updateTransition(showContent, label = "")
+
     Column(
         modifier = Modifier
+            .padding(8.dp)
             .fillMaxWidth(0.8f)
             .border(BorderStroke(1.dp, Color.White), shape = RoundedCornerShape(18.dp))
-            .background(BlueLightBack, RoundedCornerShape(18.dp)),
+            .background(BlueLightBack, RoundedCornerShape(18.dp))
+            .clickable { showContent = !showContent },
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
@@ -108,13 +129,14 @@ fun AboutUsBody() {
 
             Text(
                 text = "Book name",
-                fontSize = 18.sp,
+                fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black
             )
             Text(
                 text = "by Writer Name",
-                fontSize = 16.sp
+                fontSize = 14.sp,
+                fontWeight = FontWeight.W500
             )
         }
 
@@ -132,54 +154,82 @@ fun AboutUsBody() {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
         ) {
             Text(
                 text = "Published on: 2002",
-                fontSize = 16.sp
+                fontSize = 16.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Medium
             )
             Text(
                 text = "120 pages",
-                fontSize = 16.sp
+                fontSize = 16.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Medium
             )
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
-        Text(
-            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris ...",
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(8.dp)
-        )
+        if (showContent) {
+            val visibleState = transition.animateDp(
+                transitionSpec = {
+                    if (false isTransitioningTo true) {
+                        tween(durationMillis = 300)
+                    } else {
+                        spring()
+                    }
+                }, label = ""
+            ) { if (it) 300.dp else 0.dp }
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-        ) {
-            InfoButton(txt = "Buy", txtColor = YellowBackground) {
-                // TODO("Handle Buy button")
-            }
-            InfoButton(txt = "Preview") {
-                // TODO("Handle Preview button")
-            }
-            InfoButton(txt = "Read") {
-                // TODO("Handle Read button")
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(visibleState.value)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Text(
+                    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer orem ipsum dolor sit amet, consectetur adipiscing elit. Integer  orem ipsum dolor sit amet, consectetur adipiscing elit. Integer  orem ipsum dolor sit amet, consectetur adipiscing elit. Integer orem ipsum dolor sit amet, consectetur adipiscing elit. Integer orem ipsum dolor sit amet, consectetur adipiscing elit. Integer orem ipsum dolor sit amet, consectetur adipiscing elit. Integer orem ipsum dolor sit amet, consectetur adipiscing elit. Integer orem ipsum dolor sit amet, consectetur adipiscing elit. Integer orem ipsum dolor sit amet, consectetur adipiscing elit. Integer orem ipsum dolor sit amet, consectetur adipiscing elit. Integer orem ipsum dolor sit amet, consectetur adipiscing elit. Integer orem ipsum dolor sit amet, consectetur adipiscing elit. Integer  nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris t amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris ...",
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.padding(16.dp)
+                )
             }
         }
 
+    }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+    ) {
+        InfoButton(txt = "Buy", txtColor = YellowBackground) {
+            // TODO("Handle Buy button")
+        }
+        InfoButton(txt = "Preview") {
+            // TODO("Handle Preview button")
+        }
+        InfoButton(txt = "Read") {
+            // TODO("Handle Read button")
+        }
     }
 }
 
 @Composable
 fun InfoButton(
-    txt: String, txtColor: Color = Color.Gray, onButtonClicked: () -> Unit
+    txt: String, txtColor: Color = Color.White, onButtonClicked: () -> Unit
 ) {
-    Button(onClick = onButtonClicked, modifier = Modifier.clip(RoundedCornerShape(40))) {
+    Button(
+        onClick = onButtonClicked, modifier = Modifier
+            .size(120.dp, 60.dp)
+            .clip(RoundedCornerShape(40))
+    ) {
         Text(
-            text = txt, 
+            text = txt,
             color = txtColor,
             fontWeight = FontWeight.Medium,
             modifier = Modifier.padding(8.dp)
