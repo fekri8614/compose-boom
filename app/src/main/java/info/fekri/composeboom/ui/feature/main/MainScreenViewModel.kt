c7e6766cbd69efdeeb9c15f8d6fd4c6d60ad21cb
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-07-30 11:43:08 -0700

    added `OpenVerticalPdfScreen` and `OpenHorizontalPdfScreen`

--- app/src/main/java/info/fekri/composeboom/ui/feature/main/MainScreenViewModel.kt
+++ app/src/main/java/info/fekri/composeboom/ui/feature/main/MainScreenViewModel.kt
@@ -21,7 +21,6 @@ class MainScreenViewModel(
 
     val showProgress  = mutableStateOf(false)
     val showNetDialog = mutableStateOf(false)
-    val showInfoDialog = mutableStateOf(false)
 
     val showUiKids    = mutableStateOf(false)
     val showUiScience = mutableStateOf(false)
