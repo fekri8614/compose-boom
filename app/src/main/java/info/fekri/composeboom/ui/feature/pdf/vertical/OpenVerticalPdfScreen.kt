9500f64cbbcef44727e5c79a5ceb897f44d9229f
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-07-30 11:58:44 -0700

    open pdf process is done

--- app/src/main/java/info/fekri/composeboom/ui/feature/pdf/vertical/OpenVerticalPdfScreen.kt
+++ app/src/main/java/info/fekri/composeboom/ui/feature/pdf/vertical/OpenVerticalPdfScreen.kt
@@ -1,9 +1,15 @@
 package info.fekri.composeboom.ui.feature.pdf.vertical
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.rizzi.bouquet.ResourceType
+import com.rizzi.bouquet.VerticalPDFReader
 import com.rizzi.bouquet.rememberVerticalPdfReaderState
+import info.fekri.composeboom.ui.theme.BackgroundMain
 
 @Composable
 fun OpenVerticalPdfScreen(pdfUrl: String) {
@@ -12,4 +18,18 @@ fun OpenVerticalPdfScreen(pdfUrl: String) {
         resource = ResourceType.Remote(pdfUrl),
         isZoomEnable = true
     )
+
+    SelectionContainer(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundMain)
+    ) {
+        VerticalPDFReader(
+            state = pdfState,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(BackgroundMain)
+        )
+    }
+
 }
